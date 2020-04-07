f8a5e6db7f0c7e26d62e112fab3496bdc6d99524
Himanshu Garg <hgarg701@gmail.com>
2020-04-06 23:04:07 -0700

    Added shopping cart adapter

---
+++ java/com/example/amazonn/ShoppingCartAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.amazonn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ShoppingCartAdapter : RecyclerView.Adapter<ShoppingCartAdapter.ShoppingCartViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingCartViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ShoppingCartViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    class ShoppingCartViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+
+    }
+}
