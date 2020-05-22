4ad211f1d6c4ed4fe9f39c7a2b18d976790dda64
Himanshu Garg <hgarg701@gmail.com>
2020-05-22 08:12:33 -0700

    Removed ProductDescriptionViewModel

--- java/com/example/amazonn/ProductDescriptionViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.amazonn
-
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-
-class ProductDescriptionViewModel(val cartDao : CartProductDao, application: Application) : AndroidViewModel(application){
-
-}
